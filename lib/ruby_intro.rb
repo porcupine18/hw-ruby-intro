0279ba0b472d88b7cd7a50a6b8a1532fcdecc963
EC2 Default User <ec2-user@ip-172-31-28-253.us-east-2.compute.internal>
2021-09-16 22:59:04 +0000

    finished till part2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,133 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  
+  
+  for ele in arr
+    sum += ele
+  end
+  
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  
+  if arr.count == 0
+    return 0
+  
+  elsif arr.count == 1
+    return arr[0]
+  
+  else
+    
+    first  = arr[0]
+    second = arr[1]
+    
+    for ele in arr
+      if ele > first || ele == arr[0]
+        first = ele 
+        
+      elsif ele > second
+        second = ele
+      end
+      
+    end  
+    
+    #puts "-----#{first} & #{second}-----"
+    return (first + second)
+    
+  end
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sum = 0
+  
+  for i in arr
+    for j in arr
+      if i != j && (i+j) == n
+        puts "true #{i}+#{j}"
+        return true
+      end
+    end
+  end
+  
+  return false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s == "" || s == '' || s[0] == ' '
+    return false
+  end
+  
+  pattern = /^[^aeiouAEIOU\W]/
+  
+  if s.scan(pattern).size > 0
+    return true
+  else
+    return false
+  end
+  
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == " " || s == ' '
+    returns false
+  end
+  
+  pattern = /^[0-1]+$/
+  if s.scan(pattern).size > 0
+    if(s.to_i%4 == 0)
+      return true
+    end
+  end
+  
+  return false
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn_num, price_num)
+    if isbn_num == nil || price_num == nil || isbn_num < 0 || price_num < 0
+      raise Argument Error, 'Empty or bad argument'
+    
+    else
+      @isbn = isbn_num
+      @price = price_num
+    end
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @isbn=isbn.to_i
+  end
+  
+  def price=(price)
+    @price = price.to_i
+  end
+    
+  def price_as_string()
+    num = '%.2f' % (price)
+    puts "$#{num}"
+    return "$#{num}"
+  end
+  
 end
